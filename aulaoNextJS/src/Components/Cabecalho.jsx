a655a5d42ffa770035ce0cb9b817e28d6d483d15
Fabiano <montemezzofabiano@gmail.com>
2024-02-07 15:56:15 -0300

    07/02_03

---
+++ aulaoNextJS/src/Components/Cabecalho.jsx
@@ -0,0 +1,7 @@
+export default function Cabecalho () {
+    return (
+        <header>
+            <h1>Fundamento de Next.js & React</h1>
+        </header>
+    )
+}
---
+++ aulaoNextJS/src/app/exemplo.jsx
@@ -0,0 +1,6 @@
+import Cabecalho from "../Components/Cabecalho"
+import Link from "next/link"
+export default function Exemplo() {
+    return
+    <Cabecalho />
+}
