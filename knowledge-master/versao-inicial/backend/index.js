da8ff96b4254d18bdce54eea67e254caff009f03
Fabiano <montemezzofabiano@gmail.com>
2024-02-08 17:22:45 -0300

    08/02

--- knowledge-master/versao-inicial/backend/index.js
+++ knowledge-master/versao-inicial/backend/index.js
@@ -1,4 +1,15 @@
 const app = require('express')()
+const consign = require('consign')
+const db = require('./config/db')
+
+app.db = db
+
+consign()
+.then('./config/middlewares.js')
+.then('./api')
+.then('./config/routes.js')
+.then(app)
+
 
 app.listen(3001, () => {
     console.log('Backend executando...')
